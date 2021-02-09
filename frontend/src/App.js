cbac0e2576b358ca5880e56475d31fad0431bfb1
hgoldenberg <hgoldenberg@hotmail.com>
2021-02-09 20:18:27 -0300

    frontend and componentes schema

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Home from "./components/pages/Home.js";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+import "./style.css";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
